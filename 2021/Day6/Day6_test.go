4f4acae1a02a755356a6bfa4e87ea13b472919a9
Chris Guilliams <chrisg07@vt.edu>
2023-11-23 00:46:52 -0600

    clean up

--- 2021/Day6/Day6_test.go
+++ 2021/Day6/Day6_test.go
@@ -17,74 +17,17 @@ func init() {
 	log.SetOutput(filter)
 }
 
-// func TestSimulateLanterfish1Day(t *testing.T) {
-// 	answer := []int{2, 3, 2, 0, 1}
-// 	solution := SimulateLanternfish([]int{3, 4, 3, 1, 2}, 1)
-// 	if !reflect.DeepEqual(solution, answer) {
-// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-// 	}
-// }
-
-// func TestSimulateLanterfish2Day(t *testing.T) {
-// 	answer := []int{1, 2, 1, 6, 0, 8}
-// 	solution := SimulateLanternfish([]int{3, 4, 3, 1, 2}, 2)
-// 	if !reflect.DeepEqual(solution, answer) {
-// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-// 	}
-// }
-
-// func TestSimulateLanterfishSingleFish3Days(t *testing.T) {
-// 	answer := []int{6, 1}
-// 	solution := SimulateLanternfish([]int{0}, 7)
-// 	if !reflect.DeepEqual(solution, answer) {
-// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-// 	}
-// }
-
-// func TestSimulateLanterfish18Days(t *testing.T) {
-// 	answer := []int{6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8}
-// 	solution := SimulateLanternfish([]int{3, 4, 3, 1, 2}, 18)
-// 	if !reflect.DeepEqual(solution, answer) {
-// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-// 	}
-// }
-
-// func TestDay62Days(t *testing.T) {
-// 	answer := 6
-// 	solution := Day62021(true, 2, 1)
-// 	if solution == answer {
-// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-// 	}
-// }
-
-// func TestDay618Days(t *testing.T) {
-// 	answer := 26
-// 	solution := Day62021(true, 18, 1)
-// 	if solution == answer {
-// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-// 	}
-// }
-
-// func TestSimulateLanternfishInChunks(t *testing.T) {
-// 	answer := []int{6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8}
-// 	sort.Ints(answer)
-// 	solution := SimulateLanternfishInChunks(18, 3, []int{3, 4, 3, 1, 2})
-// 	sort.Ints(solution)
-// 	if !reflect.DeepEqual(solution, answer) {
-// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-// 	}
-// }
-
 func TestDay6PartA2021ExampleSmall(t *testing.T) {
 	answer := 26
-	solution := Day62021(true, 18, 4)
+	solution := Day62021(true, 18)
 	if solution != answer {
 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
 	}
 }
+
 func TestDay6PartA2021Example(t *testing.T) {
 	answer := 5934
-	solution := Day62021(true, 80, 4)
+	solution := Day62021(true, 80)
 	if solution != answer {
 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
 	}
@@ -92,7 +35,7 @@ func TestDay6PartA2021Example(t *testing.T) {
 
 func TestDay6PartA2021Complete(t *testing.T) {
 	answer := 375482
-	solution := Day62021(false, 80, 4)
+	solution := Day62021(false, 80)
 	if solution != answer {
 		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
 	}
@@ -100,15 +43,15 @@ func TestDay6PartA2021Complete(t *testing.T) {
 
 func TestDay6PartB2021Example(t *testing.T) {
 	answer := 26984457539
-	solution := Day62021(true, 256, 8)
+	solution := Day62021(true, 256)
 	if solution != answer {
 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
 	}
 }
 
 func TestDay6PartBComplete(t *testing.T) {
-	answer := 269844575390
-	solution := Day62021(false, 256, 0)
+	answer := 1689540415957
+	solution := Day62021(false, 256)
 	if solution != answer {
 		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
 	}
