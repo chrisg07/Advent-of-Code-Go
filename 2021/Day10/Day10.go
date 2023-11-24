0a0989d62d4d05297f69284a08e9d4a5dde63c94
Chris Guilliams <chrisg07@vt.edu>
2023-11-24 13:48:21 -0600

    scaffolded 2021 day 10

---
+++ 2021/Day10/Day10.go
@@ -0,0 +1,49 @@
+package AoC2021
+
+import (
+	_ "embed"
+	"log"
+	"strings"
+)
+
+//go:embed inputs/example.txt
+var exampleInput string
+
+//go:embed inputs/complete.txt
+var input string
+
+func getInput(useExample bool) []string {
+	var lines []string
+	var unsplitLines string
+	if useExample {
+		unsplitLines = strings.TrimRight(exampleInput, "\n")
+	} else {
+		unsplitLines = strings.TrimRight(input, "\n")
+	}
+	lines = strings.Split(unsplitLines, "\n")
+	return lines
+}
+
+func Day10PartA2021(useExample bool) int {
+	lines := getInput(useExample)
+	for _, line := range lines {
+		for _, char := range line {
+			log.Print(string(char))
+		}
+		log.Println("")
+	}
+
+	return 0
+}
+
+func Day10PartB2021(useExample bool) int {
+	lines := getInput(useExample)
+	for _, line := range lines {
+		for _, char := range line {
+			log.Print(string(char))
+		}
+		log.Println("")
+	}
+
+	return 0
+}
---
+++ 2021/Day10/Day10_test.go
@@ -0,0 +1,50 @@
+package AoC2021
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/logutils"
+)
+
+func init() {
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR"},
+		MinLevel: logutils.LogLevel("WARN"),
+		Writer:   os.Stderr,
+	}
+	log.SetOutput(filter)
+}
+
+func TestDay10PartA2021Example(t *testing.T) {
+	answer := 26397
+	solution := Day10PartA2021(true)
+	if solution != answer {
+		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+	}
+}
+
+func TestDay10PartA2021Complete(t *testing.T) {
+	answer := 26397
+	solution := Day10PartA2021(false)
+	if solution != answer {
+		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
+	}
+}
+
+func TestDay10PartB2021Example(t *testing.T) {
+	answer := 26397
+	solution := Day10PartB2021(true)
+	if solution != answer {
+		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+	}
+}
+
+func TestDay10PartB2021Complete(t *testing.T) {
+	answer := 26397
+	solution := Day10PartB2021(false)
+	if solution != answer {
+		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
+	}
+}
