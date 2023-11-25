825ad7835b603a0d33221dc9d291ea8d3e965e31
Chris Guilliams <chrisg07@vt.edu>
2023-11-24 22:18:10 -0600

    commented out tests in scaffold so that vscode will stop choking

--- scaffolds/Scaffold_test.go
+++ scaffolds/Scaffold_test.go
@@ -3,7 +3,6 @@ package AoC2021
 import (
 	"log"
 	"os"
-	"testing"
 
 	"github.com/hashicorp/logutils"
 )
@@ -17,34 +16,34 @@ func init() {
 	log.SetOutput(filter)
 }
 
-func TestDayXPartA2021Example(t *testing.T) {
-	answer := 0
-	solution := DayXPartA2021(true)
-	if solution != answer {
-		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-	}
-}
+// func TestDayXPartA2021Example(t *testing.T) {
+// 	answer := 0
+// 	solution := DayXPartA2021(true)
+// 	if solution != answer {
+// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+// 	}
+// }
 
-func TestDayXPartA2021Complete(t *testing.T) {
-	answer := 0
-	solution := DayXPartA2021(false)
-	if solution != answer {
-		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
-	}
-}
+// func TestDayXPartA2021Complete(t *testing.T) {
+// 	answer := 0
+// 	solution := DayXPartA2021(false)
+// 	if solution != answer {
+// 		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
+// 	}
+// }
 
-func TestDayXPartB2021Example(t *testing.T) {
-	answer := 0
-	solution := DayXPartB2021(true)
-	if solution != answer {
-		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-	}
-}
+// func TestDayXPartB2021Example(t *testing.T) {
+// 	answer := 0
+// 	solution := DayXPartB2021(true)
+// 	if solution != answer {
+// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+// 	}
+// }
 
-func TestDayXPartB2021Complete(t *testing.T) {
-	answer := 0
-	solution := DayXPartB2021(false)
-	if solution != answer {
-		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
-	}
-}
+// func TestDayXPartB2021Complete(t *testing.T) {
+// 	answer := 0
+// 	solution := DayXPartB2021(false)
+// 	if solution != answer {
+// 		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
+// 	}
+// }
