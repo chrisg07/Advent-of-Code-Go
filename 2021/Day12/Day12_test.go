72ca80abc459242d2a4d444a9c51db943b0068f2
Chris Guilliams <chrisg07@vt.edu>
2023-11-24 22:21:32 -0600

    2021 day 12 scaffold

---
+++ 2021/Day12/Day12_test.go
@@ -0,0 +1,50 @@
+package AoC2021
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/logutils"
+)
+
+func init() {
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR"},
+		MinLevel: logutils.LogLevel("WARN"),
+		Writer:   os.Stderr,
+	}
+	log.SetOutput(filter)
+}
+
+func TestDay12PartA2021Example(t *testing.T) {
+	answer := 10
+	solution := Day12PartA2021(true)
+	if solution != answer {
+		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+	}
+}
+
+func TestDay12PartA2021Complete(t *testing.T) {
+	answer := 10
+	solution := Day12PartA2021(false)
+	if solution != answer {
+		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
+	}
+}
+
+// func TestDayXPartB2021Example(t *testing.T) {
+// 	answer := 0
+// 	solution := DayXPartB2021(true)
+// 	if solution != answer {
+// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+// 	}
+// }
+
+// func TestDayXPartB2021Complete(t *testing.T) {
+// 	answer := 0
+// 	solution := DayXPartB2021(false)
+// 	if solution != answer {
+// 		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
+// 	}
+// }
