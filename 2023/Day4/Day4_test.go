86dc64f81fa4e3e1fa9db8ffe22d4383890df7d4
Chris Guilliams <chrisg07@vt.edu>
2023-12-03 01:10:21 -0600

    scaffolded day 4 files

---
+++ 2023/Day4/Day4_test.go
@@ -0,0 +1,50 @@
+package AoC2021
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/logutils"
+)
+
+func init() {
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR"},
+		MinLevel: logutils.LogLevel("WARN"),
+		Writer:   os.Stderr,
+	}
+	log.SetOutput(filter)
+}
+
+func TestDay4PartA2023Example(t *testing.T) {
+	answer := 0
+	solution := Day4PartA2023(true)
+	if solution != answer {
+		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+	}
+}
+
+func TestDay4PartA2023Complete(t *testing.T) {
+	answer := 0
+	solution := Day4PartA2023(false)
+	if solution != answer {
+		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
+	}
+}
+
+// func TestDay4PartB2023Example(t *testing.T) {
+// 	answer := 0
+// 	solution := Day4PartB2023(true)
+// 	if solution != answer {
+// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+// 	}
+// }
+
+// func TestDay4PartB2023Complete(t *testing.T) {
+// 	answer := 0
+// 	solution := Day4PartB2023(false)
+// 	if solution != answer {
+// 		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
+// 	}
+// }
