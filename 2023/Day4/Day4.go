f1baa8491617ee2a40c7e1f8678a6e04fe6cbe90
Chris Guilliams <chrisg07@vt.edu>
2023-12-04 08:46:46 -0600

    2023 day 3 part a

--- 2023/Day4/Day4.go
+++ 2023/Day4/Day4.go
@@ -3,6 +3,8 @@ package AoC2021
 import (
 	_ "embed"
 	"log"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -24,16 +26,57 @@ func getInput(useExample bool) []string {
 	return lines
 }
 
+type Game struct {
+	cardNumbers    []int
+	winningNumbers []int
+}
+
 func Day4PartA2023(useExample bool) int {
 	lines := getInput(useExample)
+
+	cards := []Game{}
 	for _, line := range lines {
-		for _, char := range line {
-			log.Print(string(char))
+		parts := strings.Split(line, ": ")
+		numbersStrs := strings.Split(parts[1], " | ")
+		winningNumbersStr := strings.Split(numbersStrs[0], " ")
+		winningNumbers := []int{}
+		for _, str := range winningNumbersStr {
+			str = strings.TrimSpace(str)
+			value, _ := strconv.Atoi(str)
+			winningNumbers = append(winningNumbers, value)
 		}
-		log.Println("")
+		cardNumbersStr := strings.Split(numbersStrs[1], " ")
+		cardNumbers := []int{}
+		for _, str := range cardNumbersStr {
+			str = strings.TrimSpace(str)
+			value, _ := strconv.Atoi(str)
+			cardNumbers = append(cardNumbers, value)
+		}
+		card := Game{
+			winningNumbers: winningNumbers,
+			cardNumbers:    cardNumbers,
+		}
+		cards = append(cards, card)
 	}
 
-	return 0
+	sum := 0
+	// for each game determine win
+	for index, card := range cards {
+		points := 0
+		for _, cardNumber := range card.cardNumbers {
+			if slices.Contains(card.winningNumbers, cardNumber) && cardNumber != 0 {
+				log.Printf("[WARN] Card %d has winning number %d", index, cardNumber)
+				if points == 0 {
+					points = 1
+				} else {
+					points *= 2
+				}
+			}
+		}
+		log.Printf("[WARN] Card %d won with %d points", index, points)
+		sum += points
+	}
+	return sum
 }
 
 func Day4PartB2023(useExample bool) int {
