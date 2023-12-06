39f765db30709487dd7de6f3bf6b3cc76da9f3de
Chris Guilliams <chrisg07@vt.edu>
2023-12-06 08:45:37 -0600

    2023 Day 7 scaffold' ' git push

---
+++ 2023/Day7/Day7_test.go
@@ -0,0 +1,49 @@
+package AoC2021
+
+import (
+	"log"
+	"os"
+
+	"github.com/hashicorp/logutils"
+)
+
+func init() {
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR"},
+		MinLevel: logutils.LogLevel("WARN"),
+		Writer:   os.Stderr,
+	}
+	log.SetOutput(filter)
+}
+
+// func TestDay7PartA2023Example(t *testing.T) {
+// 	answer := 0
+// 	solution := Day7PartA2023(true)
+// 	if solution != answer {
+// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+// 	}
+// }
+
+// func TestDay7PartA2023Complete(t *testing.T) {
+// 	answer := 0
+// 	solution := Day7PartA2023(false)
+// 	if solution != answer {
+// 		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
+// 	}
+// }
+
+// func TestDay7PartB2023Example(t *testing.T) {
+// 	answer := 0
+// 	solution := Day7PartB2023(true)
+// 	if solution != answer {
+// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+// 	}
+// }
+
+// func TestDay7PartB2023Complete(t *testing.T) {
+// 	answer := 0
+// 	solution := Day7PartB2023(false)
+// 	if solution != answer {
+// 		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
+// 	}
+// }
