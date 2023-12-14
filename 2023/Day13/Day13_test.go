4c9de445268572a45d615f31f28cdac272842260
Chris Guilliams <chrisg07@vt.edu>
2023-12-14 12:09:05 -0600

    Scaffold for 2023 Day 13

---
+++ 2023/Day13/Day13_test.go
@@ -0,0 +1,50 @@
+package AoC2021
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/logutils"
+)
+
+func init() {
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR"},
+		MinLevel: logutils.LogLevel("WARN"),
+		Writer:   os.Stderr,
+	}
+	log.SetOutput(filter)
+}
+
+func TestDay13PartA2023Example(t *testing.T) {
+	answer := 405
+	solution := Day13PartA2023(true)
+	if solution != answer {
+		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+	}
+}
+
+func TestDay13PartA2023Complete(t *testing.T) {
+	answer := 405
+	solution := Day13PartA2023(false)
+	if solution != answer {
+		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
+	}
+}
+
+// func TestDay13PartB2023Example(t *testing.T) {
+// 	answer := 0
+// 	solution := Day13PartB2023(true)
+// 	if solution != answer {
+// 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+// 	}
+// }
+
+// func TestDay13PartB2023Complete(t *testing.T) {
+// 	answer := 0
+// 	solution := Day13PartB2023(false)
+// 	if solution != answer {
+// 		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
+// 	}
+// }
