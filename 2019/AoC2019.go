d81372a6521b672510e786cd28385e2a509106a5
Chris Guilliams <chrisg07@vt.edu>
2024-11-17 15:14:09 -0600

    scaffolded 2019-1

---
+++ 2019/AoC2019.go
@@ -0,0 +1,12 @@
+package AoC2019
+
+import (
+	"fmt"
+
+	Day1 "github.com/chrisg07/Advent-of-Code-Go/2019/Day1"
+)
+
+func Solutions() {
+	fmt.Println("Advent of Code 2019 Solutions:")
+	Day1.PartA(false)
+}
---
+++ 2019/Day1/Day1_test.go
@@ -0,0 +1,54 @@
+package AoC2019
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/logutils"
+)
+
+func init() {
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR", "CONSOLE"},
+		MinLevel: logutils.LogLevel("WARN"),
+		Writer:   os.Stderr,
+	}
+	log.SetFlags(0)
+	log.SetOutput(filter)
+	log.Print("[CONSOLE] --------------------------\n")
+	log.Print("[CONSOLE] Advent of Code 2019 Day 1:\n")
+	log.Print("[CONSOLE] --------------------------\n")
+}
+
+func TestPartAExample(t *testing.T) {
+	answer := 1
+	solution := PartA(true)
+	if solution != answer {
+		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+	}
+}
+
+func TestPartAComplete(t *testing.T) {
+	answer := 1
+	solution := PartA(false)
+	if solution != answer {
+		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
+	}
+}
+
+func TestPartBExample(t *testing.T) {
+	answer := 1
+	solution := PartB(true)
+	if solution != answer {
+		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+	}
+}
+
+func TestPartBComplete(t *testing.T) {
+	answer := 1
+	solution := PartB(false)
+	if solution != answer {
+		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
+	}
+}
