133b1852adf719794eb8a5d94a96af82b221f31a
Chris Guilliams <chrisg07@vt.edu>
2024-11-19 21:27:21 -0600

    day 2

--- 2019/Day2/Day2_test.go
+++ 2019/Day2/Day2_test.go
@@ -3,6 +3,7 @@ package AoCScaffold
 import (
 	"log"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/hashicorp/logutils"
@@ -21,46 +22,48 @@ func init() {
 	log.Print("[CONSOLE] --------------------------\n")
 }
 
-func TestPartAExample(t *testing.T) {
-	answer := 1
-	solution := PartA(true)
-	if solution != answer {
+func TestPartASmallPrograms(t *testing.T) {
+	instructions := []int{1, 0, 0, 0, 99}
+	answer := []int{2, 0, 0, 0, 99}
+	solution := compute(instructions, 0)
+	if !reflect.DeepEqual(answer, solution) {
 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
 	}
-}
-
-func TestPartASmallPrograms(t *testing.T) {
-	answer := []int{2, 0, 0, 0, 99}
-	solution := compute(answer, 0)
-	if solution != answer {
+	instructions = []int{2, 3, 0, 3, 99}
+	answer = []int{2, 3, 0, 6, 99}
+	solution = compute(instructions, 0)
+	if !reflect.DeepEqual(answer, solution) {
+		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+	}
+	instructions = []int{2, 4, 4, 5, 99, 0}
+	answer = []int{2, 4, 4, 5, 99, 9801}
+	solution = compute(instructions, 0)
+	if !reflect.DeepEqual(answer, solution) {
+		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
+	}
+	instructions = []int{1, 1, 1, 4, 99, 5, 6, 0, 99}
+	answer = []int{30, 1, 1, 4, 2, 5, 6, 0, 99}
+	solution = compute(instructions, 0)
+	solution = compute(solution, 4)
+	if !reflect.DeepEqual(answer, solution) {
 		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
 	}
 }
 
 func TestPartAComplete(t *testing.T) {
-	answer := 1
+	answer := 3101878
 	solution := PartA(false)
 	if solution != answer {
-		t.Fatalf(`Comeplete solution = %d, should = %d`, solution, answer)
+		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
 	} else {
 		log.Printf("[CONSOLE] The value left at position 0 after the program halts is: %v", solution)
 	}
 }
 
-func TestPartBExample(t *testing.T) {
-	answer := 1
-	solution := PartB(true)
-	if solution != answer {
-		t.Fatalf(`Example solution = %d, should = %d`, solution, answer)
-	}
-}
-
 func TestPartBComplete(t *testing.T) {
-	answer := 1
+	answer := 8444
 	solution := PartB(false)
-	if solution != answer {
+	if solution != answer || solution == 2722500 || solution == 369600 {
 		t.Fatalf(`Complete solution = %d, should = %d`, solution, answer)
-	} else {
-		log.Printf("[CONSOLE] Find the Elf carrying the most Calories: %v", solution)
 	}
 }
